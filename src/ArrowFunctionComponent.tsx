75ffdfb6ad277435b367bd4ded07f04aab6f24c3
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-05-10 10:34:22 +0530

    create ArrowFunctionComponent.tsx

--- src/ArrowFunctionComponent.tsx
+++ src/ArrowFunctionComponent.tsx
@@ -1,6 +1,6 @@
 // This file is part of the React TypeScript Starter Kit
 //syntax for a arrow functional  component this design using Arrow function.
-// this is different from the functional component which is using ()=> function keyword
+// this is different from the functional component which is using ()=> , function keyword
 export const ArrowFunctionComponent = () => {
     return (
         <></>
