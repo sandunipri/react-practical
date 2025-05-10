8e1d5ddb20ee50a2d77f5956285fe32f74b88315
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-05-10 10:16:03 +0530

    create ClassComponent.tsx

---
+++ src/ClassComponent.tsx
@@ -0,0 +1,12 @@
+//import Component from "react"
+import {Component} from "react";
+
+// This is a class component that extends the React Component class
+//this is a react class component
+export class ClassComponent extends Component {
+    render() {
+        return (
+            <></>
+        );
+    }
+}
